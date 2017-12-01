67fcf15598e05e818871ededecab657509c51d2a
Michael Arrington <michaelarringtoniphone@gmail.com>
2017-12-01 12:04:49 -0800

    Add ability to search by keyword or name

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -6,14 +6,18 @@ $(document).ready(function() {
   $("#lookup").submit(function(event){
     event.preventDefault();
     $("#result").append("<p>Searching...</p>");
-    $.get(`https://api.betterdoctor.com/2016-03-01/doctors?query=cough&location=or-portland&skip=0&limit=10&user_key=${apiKey}`)
-    .then(function(response){
-      $("#result").empty();
-      console.log(response.data);
-      // $("#result").append(response.data.practices);
-    }).fail(function(error) {
-      $("#result").empty();
-      $("#result").append("<p>Failure</p>");
-    });
+    let limit = 10;
+    let searchBy = "query";
+    let searchQuery = $("#searchquery").val();
+    console.log(searchQuery);
+    // $.get(`https://api.betterdoctor.com/2016-03-01/doctors?${searchBy}=${searchQuery}&location=or-portland&skip=0&limit=${limit}&user_key=${apiKey}`)
+    // .then(function(response){
+    //   $("#result").empty();
+    //   console.log(response.data);
+    //   // $("#result").append(response.data.practices);
+    // }).fail(function(error) {
+    //   $("#result").empty();
+    //   $("#result").append("<p>Failure</p>");
+    // });
   });
 });
